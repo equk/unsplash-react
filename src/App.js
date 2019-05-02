db41b86dbe8d8ef1db8d67998e59d4838b001968
equk <equk@users.noreply.github.com>
2019-05-02 19:17:42 +0100

    added basic structure for page (search box + images section)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Unsplash Search</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Unsplash Search</h1>
+        <form>
+          <label htmlFor="searchTerm">Image Search</label>
+          <input
+            className="searchBox"
+            type="text"
+            id="searchTerm"
+            name="searchTerm"
+          />
+          <button type="submit">Search</button>
+        </form>
+        <section className="images" />
+        <div className="createdby">
+          <p>
+            created by <a href="https://github.com/equk">equk</a>
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
